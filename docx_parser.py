ed447a04e3189aaf5a9d339d5342520bc7e44f05
Imitater <mr.peterkushnir@gmail.com>
2024-10-09 21:32:50 +0300

    add some parsers func 2.0

--- docx_parser.py
+++ docx_parser.py
@@ -1,6 +1,8 @@
 from docx.table import Table
 from docx.table import _Row
 
+from typing import Optional
+
 from parser_utils import *
 
 
@@ -21,3 +23,24 @@ def get_groups_list(group_row: _Row) -> dict[str, int]:
             group_dict[cell.text] = idx
 
     return group_dict
+
+
+def get_some_setting_and_table_start_idx(table: Table) -> tuple[Optional[int], Optional[int], Optional[int]]:
+    """ получение индекса колонки дня недели, времени пары, а также
+     индекса строки с первой парой """
+
+    day_col_idx = time_col_idx = -1
+
+    for (idx, row) in enumerate(table.rows):
+        if row.cells[0].text.lower() == DAY_OF_WEEK_KEY_WORD:
+            day_col_idx = 0
+            for (cell_idx, cell) in enumerate(row.cells):
+                if cell.text.lower() == TIME_KEY_WORD:
+                    time_col_idx = cell_idx
+
+    if day_col_idx > -1:
+        for (idx, row) in enumerate(table.rows[day_col_idx + 1:]):
+            if row.cells[0].text.lower() in DAYS_OF_WEEK_LIST_KEY_WORDS:
+                return 0, time_col_idx, idx
+
+    return None, None, None
--- parser_utils.py
+++ parser_utils.py
@@ -1,1 +1,6 @@
 GROUP_KEY_WORD = "группа"
+DAY_OF_WEEK_KEY_WORD = "день недели"
+TIME_KEY_WORD = "время"
+
+DAYS_OF_WEEK_LIST_KEY_WORDS = [
+    "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"]
